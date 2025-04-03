1f8bc411bbec90046b95605704b7f3afcad4f793
EdsonCampanhao <edsonlemos0525@gmail.com>
2025-04-03 17:34:55 -0300

    webhook att

--- controllers/plansController.js
+++ controllers/plansController.js
@@ -1,12 +1,36 @@
 import { ClientsPlans } from "../models/ClientsPlans.js";
 import { ServicePlans } from "../models/ServicePlans.js";
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+const client = new MercadoPagoConfig({ accessToken: 'TEST-1518391375042209-031618-771d1f999699cb8e15b268d22961eff1-2329930160' });
+const preference = new Preference(client);
+let plans = await ServicePlans.findAll({ raw: true })
 
 
+for (const plan of plans) {
+    const pref = await preference.create({
+        body: {
+            items: [
+                {
+                    title: plan.name,
+                    quantity: 1,
+                    unit_price: Math.floor(plan.price)
+                }
+            ],
+        }
+    })
+        .then((preference) => {
+            plan.preferenceId = preference.id;
 
+        })
+        .catch();
+
+}
 
 export class plansController {
+
     static async renderPlans(req, res) {
-        let plans = await ServicePlans.findAll({ raw: true })
+
         res.render("plans", { plans: plans })
     }
     static async hasActivePlan(req, res, next) {
@@ -31,8 +55,10 @@ export class plansController {
             clientId: req.body.clientId,
         }
 
+        console.log(req.body)
+
         const newPlanContracted = await ClientsPlans.create({ serviceId: user.planId, clientId: user.clientId })
 
-        res.json({result:"plano contratado com sucesso"})
+        res.json({ result: "plano contratado com sucesso" })
     }
 }
